a0e86a6b1eff3ee8278711b5e398d1dfb04d3f90
Ankit Shaw <masterankit262003@gmail.com>
2025-07-22 13:37:36 +0530

    Initial commit

---
+++ src/components/Page1ButtomText.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Page1ButtomText = () => {
+  return (
+       <div className='absolute bottom-2 left-6 sm:left-10 px-4 py-2 rounded-md shadow-md'>
+      <div className='text-white text-sm sm:text-base leading-relaxed font-semibold text-left'>
+        <h2 className='text-3xl font-serif text-black'><span className='text-white'>Ansh Shaw |</span> KGEC |</h2>
+        <h3 className='text-3xl font-serif text-cyan-300'>JavaScript |<span className='text-white'>Python</span></h3>
+      </div>
+    </div>
+  );
+};
+
+export default Page1ButtomText;
